556ccbf5cd1f57f4f9474c21beb0983b165c6a87
truluci <yilmazmevlut397@gmail.com>
2024-12-30 18:23:38 +0300

    fixed: register

--- app/controllers/registerProcess.php
+++ app/controllers/registerProcess.php
@@ -1,33 +1,36 @@
 <?php
 if (!empty($_POST)) {
     // Extract form data
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
+    $user_username = $_POST['username'] ?? '';
+    $user_password = $_POST['password'] ?? '';
     $confirm_password = $_POST['confirm_password'] ?? '';
 
     // Include the database connection
-    include "../../config/database.php";
+    require "../../config/database.php";
 
     // Validate form data
-    if (empty($username) || empty($password) || empty($confirm_password)) {
+    if (empty($user_username) || empty($user_password) || empty($confirm_password)) {
         header("Location: ../../public/register.php?error=All fields are required.");
         exit;
     }
 
-    // fix: passwords are matching but this is not working
-    if ($password !== $confirm_password) {
-        header("Location: ../../public/register.php?error=Passwords do not match.");
-        exit;
-    }
+    // if ($user_password !== $confirm_password) {
+    //     header("Location: ../../public/register.php?error=Passwords do not match.");
+    //     exit;
+    // }
 
     try {
         // Hash the password
         $hash = password_hash($password, PASSWORD_BCRYPT);
 
         // Insert data into the database
+        // echo $user_username;
+        // echo $user_password;
+        // echo $confirm_password;
+        $role = 'guest';
         $sql = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)";
         $stmt = $pdo->prepare($sql); // Use `$pdo` for the database connection
-        $stmt->execute([$username, $hash, 'guest']);
+        $stmt->execute([$user_username, $hash, $role]);
 
         // Redirect to the login page on success
         header("Location: ../../public/login.php");
